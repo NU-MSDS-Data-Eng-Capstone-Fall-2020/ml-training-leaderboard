7825bb8976883cb606e78ca47cd3d33554399d90
herculeslyndel <38410965+herculeslyndel@users.noreply.github.com>
2020-11-22 18:33:39 -0500

    change to path.py descr in readme.md

--- assembler/get/results_training.py
+++ assembler/get/results_training.py
@@ -3,30 +3,33 @@
 import json
 
 def get_dict(fn):
+    
     with open(fn) as fin:
         read_data = json.load(fin)
-
-    trials_train = []
-    episodes_train = []
-    episode_statuses_train = []
-    completion_pcts_train = []
-    reward_scores_train = []
-    elapsed_times_train = []
+        
     metric_times_train = []
-    train_lists = {}
+    trials_train = []
+    episode_statuses_train = []
+    reward_scores_train = []
+    completion_pcts_train = []
+    elapsed_times_train = []
+    episodes_train = []
+    train_lists = OrderedDict()
+    
     for i in read_data['metrics']:
-        trials_train.append(i["trial"])
-        episodes_train.append(i["episode"])
-        episode_statuses_train.append(i["episode_status"])
-        completion_pcts_train.append(i["completion_percentage"])
-        reward_scores_train.append(i["reward_score"])
-        elapsed_times_train.append(i["elapsed_time_in_milliseconds"])
         metric_times_train.append(i["metric_time"])
-    train_lists["trials_train"] = trials_train
-    train_lists["episodes_train"] = episodes_train
-    train_lists["episode_statuses_train"] = episode_statuses_train
-    train_lists["completion_pcts_train"] = completion_pcts_train
-    train_lists["reward_scores_train"] = reward_scores_train
-    train_lists["elapsed_times_train"] = elapsed_times_train
+        trials_train.append(i["trial"])
+        episode_statuses_train.append(i["episode_status"])
+        reward_scores_train.append(i["reward_score"])
+        completion_pcts_train.append(i["completion_percentage"])
+        elapsed_times_train.append(i["elapsed_time_in_milliseconds"])
+        episodes_train.append(i["episode"])
+    
     train_lists["metric_times_train"] = metric_times_train
+    train_lists["trials_train"] = trials_train
+    train_lists["episode_statuses_train"] = episode_statuses_train
+    train_lists["reward_scores_train"] = reward_scores_train
+    train_lists["completion_pcts_train"] = completion_pcts_train
+    train_lists["elapsed_times_train"] = elapsed_times_train
+    train_lists["episodes_train"] = episodes_train
     return train_lists
